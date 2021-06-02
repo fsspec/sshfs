36e6715d62c7c950d5c9a655767a4e079ab52939
Batuhan Taskaya <batuhan@python.org>
2021-06-02 16:37:14 +0300

    project: add the license

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name="sshfs",
     version="2021.06.0",
     description="SSH File System -- SSH/SFTP backend for fsspec",
+    license="Apache License 2.0",
     install_requires=Path("requirements.txt").read_text(),
     python_modules=["sshfs"],
 )
