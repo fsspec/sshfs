6089a51382a11bd6a7a521f1f1d9209d2d01365f
Batuhan Taskaya <batuhan@python.org>
2021-06-03 13:26:41 +0300

    setup: bump the version for new module layout

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 
 setup(
     name="sshfs",
-    version="2021.06.0",
+    version="2021.06.0a1",
     description="SSH File System -- SSH/SFTP backend for fsspec",
     license="Apache License 2.0",
     install_requires=Path("requirements.txt").read_text(),
-    python_modules=["sshfs"],
+    py_modules=["sshfs"],
 )
