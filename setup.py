eddccf69abe6e867a451df57dff707412c3c6a4b
Batuhan Taskaya <batuhan@python.org>
2021-06-07 14:54:19 +0300

    gh: auto publish packages

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-from pathlib import Path
-
 from setuptools import setup
 
-setup(
-    name="sshfs",
-    version="2021.06.0a11",
-    description="SSH Filesystem -- Async SSH/SFTP backend for fsspec",
-    license="Apache License 2.0",
-    install_requires=Path("requirements.txt").read_text().splitlines(),
-    long_description=Path("README.md").read_text(),
-    long_description_content_type="text/markdown",
-    py_modules=["sshfs"],
-    extras_require={
-        "bcrypt": ["asyncssh[bcrypt]"],
-        "fido2": ["asyncssh[fido2]"],
-        "gssapi": ["asyncssh[gssapi]"],
-        "libnacl": ["asyncssh[libnacl]"],
-        "pkcs11": ["asyncssh[python-pkcs11]"],
-        "pyOpenSSL": ["asyncssh[pyOpenSSL]"],
-        "pywin32": ["asyncssh[pywin32]"],
-    },
-)
+setup()
