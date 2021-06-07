0eb19f5bb8150d7d6a8cd619a0f81eb62ea95c98
Batuhan Taskaya <batuhan@python.org>
2021-06-07 14:26:31 +0300

    setup: add extra options for gss etc.

--- setup.py
+++ setup.py
@@ -7,8 +7,17 @@ setup(
     version="2021.06.0a11",
     description="SSH Filesystem -- Async SSH/SFTP backend for fsspec",
     license="Apache License 2.0",
-    install_requires=Path("requirements.txt").read_text(),
+    install_requires=Path("requirements.txt").read_text().splitlines(),
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
     py_modules=["sshfs"],
+    extras_require={
+        "bcrypt": ["asyncssh[bcrypt]"],
+        "fido2": ["asyncssh[fido2]"],
+        "gssapi": ["asyncssh[gssapi]"],
+        "libnacl": ["asyncssh[libnacl]"],
+        "pkcs11": ["asyncssh[python-pkcs11]"],
+        "pyOpenSSL": ["asyncssh[pyOpenSSL]"],
+        "pywin32": ["asyncssh[pywin32]"],
+    },
 )
