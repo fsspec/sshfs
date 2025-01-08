da82e76a733b3cd7f378fb32a44c80fd948444c7
Sailesh <58628378+99-NinetyNine@users.noreply.github.com>
2025-01-08 19:01:44 +0545

    Update config.py to be compatible with SSHClientConfig load method

--- sshfs/config.py
+++ sshfs/config.py
@@ -1,12 +1,11 @@
 import getpass
 from contextlib import suppress
-from pathlib import Path
-
+from pathlib import Path, PurePath
+from typing import Sequence, Union
 from asyncssh.config import SSHClientConfig
 
 SSH_CONFIG = Path("~", ".ssh", "config").expanduser()
-
-
+FilePath = Union[str, PurePath]
 def parse_config(
     *, host, user=(), port=(), local_user=None, config_files=None
 ):
@@ -19,13 +18,24 @@ def parse_config(
 
     last_config = None
     reload = False
-
-    return SSHClientConfig.load(
-        last_config,
-        config_files,
-        reload,
-        local_user,
-        user,
-        host,
-        port,
+    config = SSHClientConfig(
+        last_config =last_config,
+        reload = reload,
+        canonical = False,
+        final=False,
+        local_user = local_user,
+        user = user,
+        host = host,
+        port = port,
     )
+
+    if config_files:
+        if isinstance(config_files, (str, PurePath)):
+            paths: Sequence[FilePath] = [config_files]
+        else:
+            paths = config_files
+
+        for path in paths:
+            config.parse(Path(path))
+        config.loaded = True
+    return config
